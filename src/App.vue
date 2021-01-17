f769b0254dfa5481e4385462275eec2d09e5d97d
Strixis <nivem-strixis@mail.ru>
2021-01-17 13:16:43 +0300

    Сделана настройка проекта

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img :src="logo" alt="logo">
+    <span class="test">Hello</span>
+    <Test />
+  </div>
+</template>
+
+<script>
+import Test from 'components/Test';
+import logo from 'img/logo-mini.svg';
+
+export default {
+  components: {
+    Test
+  },
+  data() {
+    return {
+      logo
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@use 'styles'
+.test
+  color: red
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <span class="test_component">Test component</span>
+  </div>
+</template>
