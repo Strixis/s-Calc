a411d2e6e66379211287d507f802571101629166
Strixis <nivem-strixis@mail.ru>
2021-01-21 15:47:42 +0300

    Сделана логика для некоторых поля
    Изменения:
      * Сделана логика для поля ввода названия операции
      * Сделана логика для поля ввода расценки
      * Сделана логика для поля ввода количества
      * Сделан подсчет общего количества

--- src/components/BlockMain.vue
+++ src/components/BlockMain.vue
@@ -1,7 +1,10 @@
 <template>
   <main class="main">
     <div class="calculate-form">
-      <ComponentOperationForm />
+      <ComponentOperationForm v-for="(operation, index) of operations"
+        :key="index"
+        v-bind.sync="operations[index]"
+      />
       <button class="button_add-operation">Добавить</button>
     </div>
     <div class="total">
@@ -17,6 +20,18 @@ import ComponentOperationForm from 'components/ComponentOperationForm';
 export default {
   components: {
     ComponentOperationForm,
+  },
+  data() {
+    return {
+      operations: [
+        {
+          name: 'Operation 1',
+          price: 0,
+          quantitys: [0],
+          cost: 0,
+        },
+      ],
+    };
   },
 }
 </script>
--- src/components/ComponentOperationForm.vue
+++ src/components/ComponentOperationForm.vue
@@ -1,5 +1,67 @@
 <template>
   <div>
-    <span>This is operation form</span>
+    <div>
+      <label>
+        <span>Наименование:</span>
+        <input type="text"
+          :value="name"
+          @input="$emit('update:name', $event.target.value)"
+        >
+      </label>
+      <label>
+        <span>Расценка:</span>
+        <input type="text"
+          :value="price"
+          @input="$emit('update:price', $event.target.value)"
+        >
+      </label>
+      <label>
+        <span>Количество:</span>
+        <div v-for="(quantity, index) of quantitys"
+          :key="index"
+        >
+          <input type="text"
+            :value="quantity"
+            @input="$emit('update:quantitys', 
+              changeQuantity($event.target.value, quantitys, index)
+            )"
+          >
+          <button>-</button>
+        </div>
+      </label>
+      <button>+</button>
+      <button>X</button>
+    </div>
+    <div>
+      <p>Наименование: <span>{{ name }}</span></p>
+      <p>Расценка: <span>{{ price }}</span></p>
+      <p>Общее количество: <span>{{ fullQuantity }}</span></p>
+      <p>Сумма: <span>{{ cost }}</span></p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['name', 'price', 'quantitys', 'cost'],
+  methods: {
+    changeQuantity(newValue, quantitys, index) {
+      let newQuantitys = [...quantitys];
+      newQuantitys[index] = newValue;
+      return newQuantitys;
+    },
+  },
+  computed: {
+    fullQuantity: function() {
+      return this.$props.quantitys.reduce(
+        (accumulator, currentValue) => {
+          return (
+            +currentValue || currentValue == 0 ?
+              (accumulator + +currentValue) :
+              'Количество должно быть числом.'
+          )
+        }, 0);
+    },
+  },
+}
+</script>
