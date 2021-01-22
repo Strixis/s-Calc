91b0902c9e9b0a0f982e4df738b3b10a889a55d7
Strixis <nivem-strixis@mail.ru>
2021-01-22 19:59:33 +0300

    Обновлен адаптив

--- src/components/BlockMain.vue
+++ src/components/BlockMain.vue
@@ -5,14 +5,13 @@
         :key="operation.id"
         :id="operation.id"
       />
-      <button v-if="isMobile" class="button_add-operation" @click="addNewOperation">Добавить</button>
     </div>
     <div class="total">
       <span class="total_text">Итого:</span>
       <span class="total_number">{{ totalCost }}</span>
       <span v-show="isValid" class="total_error">Расценка и количество должны быть числами.</span>
       <span v-show="isValid" class="total_error">Дробная часть разделяется точкой.</span>
-      <button v-if="!isMobile" class="button_add-operation" @click="addNewOperation">Добавить</button>
+      <button class="button_add-operation" @click="addNewOperation">Добавить</button>
     </div>
   </main>
 </template>
@@ -25,11 +24,6 @@ import { mapState, mapMutations } from 'vuex';
 export default {
   components: {
     ComponentOperationForm,
-  },
-  data() {
-    return {
-      isMobile: true,
-    };
   },
   computed: {
     ...mapState({
@@ -48,16 +42,6 @@ export default {
     ...mapMutations([
       'addNewOperation',
     ]),
-    handleResize() {
-      window.innerWidth > 767 ? this.isMobile = false : this.isMobile = true;
-    },
-  },
-  created() {
-    this.handleResize();
-    window.addEventListener('resize', this.handleResize);
-  },
-  destroyed() {
-    window.removeEventListener('resize', this.handleResize);
   },
 }
 </script>
